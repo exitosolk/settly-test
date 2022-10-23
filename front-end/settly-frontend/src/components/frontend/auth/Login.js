1be83f78424e4d1e7323f810f677489fff6701ba
exitosolk <illeperumasc@gmail.com>
2022-10-23 10:28:07 +0530

    implementing login component

--- front-end/settly-frontend/src/components/frontend/auth/Login.js
+++ front-end/settly-frontend/src/components/frontend/auth/Login.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Navbar from '../../../layouts/frontend/Navbar';
 
 function Login (){
+    
+    const [loginInput, setLogin] = useState({
+        email:'',
+        password:'',
+    });
+
+    const handleInput = (e) =>{
+        e.persist();
+        setLogin({...loginInput,[e.target.name]:e.target.value});
+    }
+
+    const loginSubmit = (e) =>{
+        e.preventDefault();
+        const data = {
+            email: loginInput.email,
+            password:loginInput.password,
+        }
+
+        axios.post(`api/login`, data).then(res=>{
+
+        })
+
     return (
         <div>
             <Navbar></Navbar>
@@ -13,12 +36,12 @@ function Login (){
                                 <h4>Login</h4>
                             </div>
                             <div className='card-body'>
-                                <form>
+                                <form onSubmit={loginSubmit}>
                                     <div className='form-group mb-3'>
-                                        <input type='email' name='email' placeholder='Email Address' className='form-control' ></input>
+                                        <input type='email' name='email' onChange={handleInput} value={loginInput.email} placeholder='Email Address' className='form-control' ></input>
                                     </div>
                                     <div className='form-group mb-3'>
-                                        <input type='password' name='password' placeholder='Password' className='form-control' ></input>
+                                        <input type='password' name='password' onChange={handleInput} value={loginInput.password} placeholder='Password' className='form-control' ></input>
                                     </div>
                                     <div className='form-group mb-3'>
                                         <button type='submit' className='btn btn-primary w-100'>Login</button>
